793ece4b0e16e7203e9551c34fc2d586d5663ba0
Sanjida Islam Sharna <sanjidaislamsharna20@gmail.com>
2025-10-31 21:21:45 +0600

    Update main.js

--- asset/js/main.js
+++ asset/js/main.js
@@ -23,7 +23,7 @@ window.onload = function(){
 
 async function getCategories(){
     try{
-        const response = await fetch('../data/categories.json');
+        const response = await fetch('https://sanjidaislamsharna.github.io/Green-Earth/asset/data/categories.json');
         const data = await response.json();
         const categories = data.categories
 
@@ -41,7 +41,7 @@ async function getCategories(){
 
 async function getPlantByCategory(categoryName){
     try{
-        const response = await fetch(`../asset/data/allplants.json`);
+        const response = await fetch(`https://sanjidaislamsharna.github.io/Green-Earth/asset/data/allplants.json`);
         const data = await response.json();
         const plants = data.plants;
 
@@ -74,7 +74,7 @@ async function getPlantByCategory(categoryName){
 
 async function loadAllPlants() {
     try{
-        const response = await fetch('../asset/data/allplants.json');
+        const response = await fetch('https://sanjidaislamsharna.github.io/Green-Earth/asset/data/allplants.json');
         const data = await response.json();
         const plants = data.plants;
 
@@ -106,7 +106,7 @@ async function loadAllPlants() {
 
 async function getPlantDetails(id){
     try{
-        const response = await fetch(`../asset/data/allplants.json`);
+        const response = await fetch(`https://sanjidaislamsharna.github.io/Green-Earth/asset/data/allplants.json`);
         const data = await response.json();
         const plant = data.plants;
 
